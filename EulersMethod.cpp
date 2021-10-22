879a45f07b80959d5a2f30211923f83bc031d76c
Chinmoy Mondal <5gsanmoy@gmail.com>
2021-10-22 23:38:14 +0530

    My IIT(ISM) 1st sem CPP folder

--- EulersMethod.cpp
+++ EulersMethod.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 using namespace std;
 
 float f(float x,float y){
@@ -25,13 +26,25 @@ int main(){
     cout<<"--- Modified Eulers's Method ---\n";cout<<endl;
     cout<<"Give the nuymber of values of x => ";int m;cin>>m;cout<<endl;
     cout<<"Enter the value of h => ";float h1;cin>>h1;cout<<endl;
-    float x1[n+1],y1[n+1];
-    cout<<"Enter the initial value of x => ";cin>>x[0];
-    cout<<"Enter the initial value of y => ";cin>>y[0];
+    float x1[m],y1[m][m+1];
     cout<<"Enter the values of x => \n";
-    for(int i=1;i<n+1;i++){
-        cin>>x[i];
+    for(int i=1;i<m+1;i++){
+        cin>>x1[i];
     }cout<<endl;
+    float nX=x[0],nY=y[0];
+    float temp;
+    for(int i=1;i<m+1;i++){
+        y1[i][0]=nY+h1*f(nX,nY);
+        cout<<"For X= "<<x1[i]<<endl;
+        cout<<"     Y"<<i<<"^("<<0<<") = "<<y1[i][0]<<endl;
+        for(int j=1;j<=m+1;j++){
+            y1[i][j]=nY+((h1/2)*(f(nX,nY)+f(x1[i],y1[i][j-1])));
+            cout<<"     Y"<<i<<"^("<<j<<") = "<<y1[i][j]<<endl;
+            temp=y1[i][j];
+        }cout<<endl;
+        nX=x1[i];
+        nY=temp;
+    }
 
     return 0;
 }
